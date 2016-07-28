e8bed68ad6cd2808113084a02c0f9a8491eb9ef5
Hongchan Choi <hongchan@chromium.org>
2016-07-28 08:37:33 -0700

    updated version to 0007

--- src/js/demo-player.js
+++ src/js/demo-player.js
@@ -24,7 +24,7 @@
 window.OmnitoneDemoPlayer = (function () {
 
   // Player information
-  var VERSION = '0.1.4.0005';
+  var VERSION = '0.1.4.0007';
 
   // The GL video mapping is 180 degree opposite.
   // TODO: fix this in vr-panorama.js
