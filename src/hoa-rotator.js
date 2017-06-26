d5b9debd51b6e528a9a09ab17f12007b7aa7e5e1
Andrew Allen <drewbitllama@gmail.com>
2017-06-26 09:58:41 -0700

    Support for Higher-Order Ambisonics (HOA). Rotation, convolution and rendering classes, w/ example.

--- src/hoa-rotator.js
+++ src/hoa-rotator.js
@@ -24,37 +24,64 @@
 /**
  * @class Higher-order-ambisonic decoder based on gain node network.
  *        We expect the order of the channels to conform to ACN ordering.
+ *        Below are the helper methods to compute SH rotation using recursion.
+ *        The code uses maths described in the following papers:
+ *
+ *        [1]  R. Green, "Spherical Harmonic Lighting: The Gritty Details",
+ *             GDC 2003,
+ *          http://www.research.scea.com/gdc2003/spherical-harmonic-lighting.pdf
+ *        [2]  J. Ivanic and K. Ruedenberg, "Rotation Matrices for Real
+ *             Spherical Harmonics. Direct Determination by Recursion", J. Phys.
+ *             Chem., vol. 100, no. 15, pp. 6342-6347, 1996.
+ *             http://pubs.acs.org/doi/pdf/10.1021/jp953350u
+ *        [2b] Corrections to initial publication:
+ *             http://pubs.acs.org/doi/pdf/10.1021/jp9833350
+ *
  * @param {AudioContext} context    Associated AudioContext.
- * @param {Number} order            Ambisonic order (2 or 3).
+ * @param {Number} ambisonicOrder   Ambisonic order (2 or 3).
  */
-function HOARotator (context, order) {
+function HOARotator (context, ambisonicOrder) {
   this._context = context;
 
-  var num_channels = (order + 1) * (order + 1);
-  this._splitter = this._context.createChannelSplitter(num_channels);
-  this._merger = this._context.createChannelMerger(num_channels);
+  /**
+   * We need to determine the number of channels K based on the ambisonic
+   * order N where
+   *   K = (N + 1)^2
+   */
+  var numChannels = (ambisonicOrder + 1) * (ambisonicOrder + 1);
+
+  this._splitter = this._context.createChannelSplitter(numChannels);
+  this._merger = this._context.createChannelMerger(numChannels);
 
   // Create a set of per-order rotation matrices using gain nodes.
-  this._m = Array(order);
-  for (var l = 1; l <= order; l++) {
-    var order_offset = l * l;
-    var rows = (2 * l + 1);
-    this._m[l - 1] = Array(rows * rows);
+  this._matrix = [];
+  for (var i = 1; i <= ambisonicOrder; i++) {
+    /**
+     * Each ambisonic order requires a separate (2l + 1) x (2l + 1) rotation
+     * matrix. We compute the offset value as the first channel index of the
+     * current order where
+     *   k_last = l^2 + l + m,
+     * and let m = -l
+     *   k_last = l^2
+     */
+    var orderOffset = i * i;
+    var rows = (2 * i + 1);
 
+    this._matrix[i - 1] = [];
     for (var j = 0; j < rows; j++) {
-      var input_index = order_offset + j;
+      var inputIndex = orderOffset + j;
       for (var k = 0; k < rows; k++) {
-        var output_index = order_offset + k;
-        var matrix_index = j * rows + k; // Row-wise indexing.
+        var outputIndex = orderOffset + k;
+        var matrixIndex = j * rows + k; // Row-wise indexing.
 
-        this._m[l - 1][matrix_index] = this._context.createGain();
-        this._splitter.connect(this._m[l - 1][matrix_index], input_index);
-        this._m[l - 1][matrix_index].connect(this._merger, 0, output_index);
+        this._matrix[i - 1][matrixIndex] = this._context.createGain();
+        this._splitter.connect(this._matrix[i - 1][matrixIndex], inputIndex);
+        this._matrix[i - 1][matrixIndex].connect(this._merger, 0, outputIndex);
       }
     }
   }
 
-  // W-channel requires not transform.
+  // W-channel is not involved in rotation, skip straight to ouput.
   this._splitter.connect(this._merger, 0, 0);
 
   // Default Identity matrix.
@@ -65,38 +92,56 @@ function HOARotator (context, order) {
   this.output = this._merger;
 };
 
+/**
+ * Kronecker Delta function.
+ * @param {Number} i
+ * @param {Number} j
+ * @return {Number}
+ */
 function _kroneckerDelta(i, j) {
   return (i == j ? 1 : 0);
 };
 
-function _computeUVWCoeff(m, n, l) {
-  var recip_denom, u, v, w;
+/**
+ * [2] uses an odd convention of referring to the rows and columns using
+ * centered indices, so the middle row and column are (0, 0) and the upper
+ * left would have negative coordinates.
+ *
+ * This is a convenience function to allow us to access a matrix array
+ * in the same manner, assuming it is a (2l+1)x(2l+1) matrix.
+ */
 
-  var d = _kroneckerDelta(m, 0);
-  if (Math.abs(n) == l) {
-    recip_denom = 1 / (2 * l * (2 * l - 1));
-  } else {
-    recip_denom = 1 / ((l + n) * (l - n));
-  }
-
-  u = Math.sqrt((l + m) * (l - m) * recip_denom);
-  v = 0.5 * (1 - 2 * d) * Math.sqrt((1 + d) * (l + Math.abs(m) - 1) *
-      (l + Math.abs(m)) * recip_denom);
-  w = -0.5 * (1 - d) *
-      Math.sqrt((l - Math.abs(m) - 1) * (l - Math.abs(m)) * recip_denom);
-  return [u, v, w];
-};
-
+/**
+ * @param {Number} l
+ * @param {Number} i
+ * @param {Number} j
+ * @param {Number} val
+ */
 HOARotator.prototype._setCenteredElement = function (l, i, j, val) {
   var index = (j + l) * (2 * l + 1) + (i + l); // Row-wise indexing.
-  this._m[l - 1][index].gain.value = val;
+  this._matrix[l - 1][index].gain.value = val;
 };
 
+/**
+ * @param {Number} l
+ * @param {Number} i
+ * @param {Number} j
+ */
 HOARotator.prototype._getCenteredElement = function (l, i, j) {
   var index = (j + l) * (2 * l + 1) + (i + l); // Row-wise indexing.
-  return this._m[l - 1][index].gain.value;
+  return this._matrix[l - 1][index].gain.value;
 };
 
+/**
+ * Helper function defined in [2] that is used by the functions U, V, W.
+ * This should not be called on its own, as U, V, and W (and their coefficients)
+ * select the appropriate matrix elements to access arguments |a| and |b|.
+ *
+ * @param {Number} i
+ * @param {Number} a
+ * @param {Number} b
+ * @param {Number} l
+ */
 HOARotator.prototype._P = function (i, a, b, l) {
   if (b == l) {
     return this._getCenteredElement(1, i, 1) *
@@ -114,10 +159,32 @@ HOARotator.prototype._P = function (i, a, b, l) {
   }
 };
 
+/**
+ * The functions U, V, and W should only be called if the correspondingly
+ * named coefficient u, v, w from the function ComputeUVWCoeff() is non-zero.
+ * When the coefficient is 0, these would attempt to access matrix elements that
+ * are out of bounds. The vector of rotations, |r|, must have the |l - 1|
+ * previously completed band rotations. These functions are valid for |l >= 2|.
+ */
+
+/**
+ * @param {Number} m
+ * @param {Number} n
+ * @param {Number} l
+ */
 HOARotator.prototype._U = function (m, n, l) {
+  /**
+   * Although [1, 2] split U into three cases for m == 0, m < 0, m > 0
+   * the actual values are the same for all three cases.
+   */
   return this._P(0, m, n, l);
 };
 
+/**
+ * @param {Number} m
+ * @param {Number} n
+ * @param {Number} l
+ */
 HOARotator.prototype._V = function (m, n, l) {
   if (m == 0) {
     return this._P(1, 1, n, l) + this._P(-1, -1, n, l);
@@ -126,14 +193,27 @@ HOARotator.prototype._V = function (m, n, l) {
     return this._P(1, m - 1, n, l) * Math.sqrt(1 + d) -
            this._P(-1, -m + 1, n, l) * (1 - d);
   } else {
+    /**
+     * Note there is apparent errata in [1,2,2b] dealing with this particular
+     * case. [2b] writes it should be P*(1-d)+P*(1-d)^0.5
+     * [1] writes it as P*(1+d)+P*(1-d)^0.5, but going through the math by hand,
+     * you must have it as P*(1-d)+P*(1+d)^0.5 to form a 2^.5 term, which
+     * parallels the case where m > 0.
+     */
     var d = _kroneckerDelta(m, -1);
     return this._P(1, m + 1, n, l) * (1 - d) +
            this._P(-1, -m - 1, n, l) * Math.sqrt(1 + d);
   }
 };
 
+/**
+ * @param {Number} m
+ * @param {Number} n
+ * @param {Number} l
+ */
 HOARotator.prototype._W = function (m, n, l) {
   if (m == 0) {
+    // Whenever this happens, w is also 0 so W can be anything.
     return 0;
   } else if (m > 0) {
     return this._P(1, m + 1, n, l) + this._P(-1, -m - 1, n, l);
@@ -142,11 +222,54 @@ HOARotator.prototype._W = function (m, n, l) {
   }
 };
 
+/**
+ * Calculates the coefficients applied to the U, V, and W functions. Because
+ * their equations share many common terms they are computed simultaneously.
+ *
+ * @param {*} m
+ * @param {*} n
+ * @param {*} l
+ */
+function _computeUVWCoeff(m, n, l) {
+  var reciprocalDenominator, u, v, w;
+
+  var d = _kroneckerDelta(m, 0);
+  if (Math.abs(n) == l) {
+    reciprocalDenominator = 1 / (2 * l * (2 * l - 1));
+  } else {
+    reciprocalDenominator = 1 / ((l + n) * (l - n));
+  }
+
+  u = Math.sqrt((l + m) * (l - m) * reciprocalDenominator);
+  v = 0.5 * (1 - 2 * d) * Math.sqrt((1 + d) * (l + Math.abs(m) - 1) *
+      (l + Math.abs(m)) * reciprocalDenominator);
+  w = -0.5 * (1 - d) * Math.sqrt((l - Math.abs(m) - 1) * (l - Math.abs(m)) *
+      reciprocalDenominator);
+  return [u, v, w];
+};
+
+/**
+ * Calculates the (2l+1)x(2l+1) rotation matrix for the band l.
+ * This uses the matrices computed for band 1 and band l-1 to compute the
+ * matrix for band l. |rotations| must contain the previously computed l-1
+ * rotation matrices.
+ *
+ * This implementation comes from p. 5 (6346), Table 1 and 2 in [2] taking
+ * into account the corrections from [2b].
+ */
 HOARotator.prototype._computeBandRotation = function (l) {
+  /**
+   * The lth band rotation matrix has rows and columns equal to the number of
+   * coefficients within that band (-l <= m <= l implies 2l + 1 coefficients).
+   */
   for (var m = -l; m <= l; m++) {
     for (var n = -l; n <= l; n++) {
       var uvw = _computeUVWCoeff(m, n, l);
 
+      /**
+       * The functions U, V, W are only safe to call if the coefficients
+       * u, v, w are not zero.
+       */
       if (Math.abs(uvw[0]) > 0) {
         uvw[0] *= this._U(m, n, l);
       }
@@ -162,10 +285,13 @@ HOARotator.prototype._computeBandRotation = function (l) {
   }
 };
 
+/**
+ * Compute the HOA rotation matrix after setting the transform matrix.
+ */
 HOARotator.prototype._computeHOAMatrices = function () {
   // We start by computing the 2nd-order matrix from the 1st-order matrix.
-  for (var l = 2; l <= this._m.length; l++) {
-    this._computeBandRotation(l);
+  for (var i = 2; i <= this._matrix.length; i++) {
+    this._computeBandRotation(i);
   }
 };
 
@@ -176,7 +302,7 @@ HOARotator.prototype._computeHOAMatrices = function () {
  */
 HOARotator.prototype.setRotationMatrix = function (rotationMatrix) {
   for (var i = 0; i < 9; i++) {
-    this._m[0][i].gain.value = rotationMatrix[i];
+    this._matrix[0][i].gain.value = rotationMatrix[i];
   }
   this._computeHOAMatrices();
 };
@@ -187,9 +313,9 @@ HOARotator.prototype.setRotationMatrix = function (rotationMatrix) {
  */
 HOARotator.prototype.setRotationMatrix4 = function (rotationMatrix4) {
   for (var i = 0; i < 12; i = i + 4) {
-    this._m[0][i].gain.value = rotationMatrix4[i];
-    this._m[0][i + 1].gain.value = rotationMatrix4[i + 1];
-    this._m[0][i + 2].gain.value = rotationMatrix4[i + 2];
+    this._matrix[0][i].gain.value = rotationMatrix4[i];
+    this._matrix[0][i + 1].gain.value = rotationMatrix4[i + 1];
+    this._matrix[0][i + 2].gain.value = rotationMatrix4[i + 2];
   }
   this._computeHOAMatrices();
 };
@@ -202,29 +328,9 @@ HOARotator.prototype.setRotationMatrix4 = function (rotationMatrix4) {
 HOARotator.prototype.getRotationMatrix = function () {
   var rotationMatrix = Float32Array(9);
   for (var i = 0; i < 9; i++) {
-    rotationMatrix[i] = this._m[0][i].gain.value;
+    rotationMatrix[i] = this._matrix[0][i].gain.value;
   }
   return rotationMatrix;
 };
-
-HOARotator.prototype.testMultiply = function (input) {
-  var output = new Float32Array(input.length);
-  var order = Math.round(Math.sqrt(input.length)) - 1;
-  output[0] = 1;
-  for (var l = 1; l <= order; l++) {
-    var order_offset = l * l;
-    for (var m = -l; m <= l; m++) {
-      var acn_index = l * l + l + m;
-      var row_length = 2 * l + 1;
-      for (var i = 0; i < row_length; i++) {
-        var col = m + l;
-        var matrix_index = col * row_length + i;
-        output[order_offset + i] +=
-          this._m[l - 1][matrix_index].gain.value * input[acn_index];
-      }
-    }
-  }
-  console.log(output);
-}
 
 module.exports = HOARotator;
