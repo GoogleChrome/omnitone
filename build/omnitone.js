f3976e3da4382912cdaf56252418920436af665f
Andrew Allen <drewbitllama@gmail.com>
2017-08-11 15:12:14 -0700

    Use direct matrix transform from camera.
    Skip inverse function (removed Utils function).
    Use camera.matrix.elements directly instead.

--- build/omnitone.js
+++ build/omnitone.js
@@ -424,76 +424,6 @@ return /******/ (function(modules) { // webpackBootstrap
 	};
 
 
-	// Static temp storage for matrix inversion.
-	var a00, a01, a02, a03, a10, a11, a12, a13;
-	var a20, a21, a22, a23, a30, a31, a32, a33;
-	var b00, b01, b02, b03, b04, b05, b06, b07, b08, b09, b10, b11;
-	var det;
-
-
-	/**
-	 * A 4x4 matrix inversion utility. This does not handle the case when the
-	 * arguments are not proper 4x4 matrices.
-	 * @param {Float32Array} out   The inverted result.
-	 * @param {Float32Array} a     The source matrix.
-	 * @returns {Float32Array} out
-	 */
-	exports.invertMatrix4 = function(out, a) {
-	  a00 = a[0];
-	  a01 = a[1];
-	  a02 = a[2];
-	  a03 = a[3];
-	  a10 = a[4];
-	  a11 = a[5];
-	  a12 = a[6];
-	  a13 = a[7];
-	  a20 = a[8];
-	  a21 = a[9];
-	  a22 = a[10];
-	  a23 = a[11];
-	  a30 = a[12];
-	  a31 = a[13];
-	  a32 = a[14];
-	  a33 = a[15];
-	  b00 = a00 * a11 - a01 * a10;
-	  b01 = a00 * a12 - a02 * a10;
-	  b02 = a00 * a13 - a03 * a10;
-	  b03 = a01 * a12 - a02 * a11;
-	  b04 = a01 * a13 - a03 * a11;
-	  b05 = a02 * a13 - a03 * a12;
-	  b06 = a20 * a31 - a21 * a30;
-	  b07 = a20 * a32 - a22 * a30;
-	  b08 = a20 * a33 - a23 * a30;
-	  b09 = a21 * a32 - a22 * a31;
-	  b10 = a21 * a33 - a23 * a31;
-	  b11 = a22 * a33 - a23 * a32;
-	  det = b00 * b11 - b01 * b10 + b02 * b09 + b03 * b08 - b04 * b07 + b05 * b06;
-
-	  if (!det)
-	    return null;
-
-	  det = 1.0 / det;
-	  out[0] = (a11 * b11 - a12 * b10 + a13 * b09) * det;
-	  out[1] = (a02 * b10 - a01 * b11 - a03 * b09) * det;
-	  out[2] = (a31 * b05 - a32 * b04 + a33 * b03) * det;
-	  out[3] = (a22 * b04 - a21 * b05 - a23 * b03) * det;
-	  out[4] = (a12 * b08 - a10 * b11 - a13 * b07) * det;
-	  out[5] = (a00 * b11 - a02 * b08 + a03 * b07) * det;
-	  out[6] = (a32 * b02 - a30 * b05 - a33 * b01) * det;
-	  out[7] = (a20 * b05 - a22 * b02 + a23 * b01) * det;
-	  out[8] = (a10 * b10 - a11 * b08 + a13 * b06) * det;
-	  out[9] = (a01 * b08 - a00 * b10 - a03 * b06) * det;
-	  out[10] = (a30 * b04 - a31 * b02 + a33 * b00) * det;
-	  out[11] = (a21 * b02 - a20 * b04 - a23 * b00) * det;
-	  out[12] = (a11 * b07 - a10 * b09 - a12 * b06) * det;
-	  out[13] = (a00 * b09 - a01 * b07 + a02 * b06) * det;
-	  out[14] = (a31 * b01 - a30 * b03 - a32 * b00) * det;
-	  out[15] = (a20 * b03 - a21 * b01 + a22 * b00) * det;
-
-	  return out;
-	};
-
-
 	/**
 	 * Get a total number of channels for a given ambisonic order.
 	 * @param {Number} order Ambisonic order
@@ -1520,7 +1450,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	        resolve();
 	      }.bind(this),
 	      function (buffers) {
-	        var errorMessage = 'Initialization failed: ' + key + ' is ' 
+	        var errorMessage = 'Initialization failed: ' + key + ' is '
 	            + buffers.get(0) + '.';
 	        Utils.log(errorMessage);
 	        reject(errorMessage);
@@ -1558,16 +1488,13 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	/**
 	 * Update the rotation matrix from a Three.js camera object.
-	 * @param  {Object} cameraMatrix      The Matrix4 obejct of Three.js the camera.
+	 * @param  {Object} camera            The Three.js camera obejct.
 	 */
-	FOARenderer.prototype.setRotationMatrixFromCamera = function (cameraMatrix) {
+	FOARenderer.prototype.setRotationMatrixFromCamera = function (camera) {
 	  if (!this._isRendererReady)
 	    return;
 
-	  // Extract the inner array elements and inverse. (The actual view rotation is
-	  // the opposite of the camera movement.)
-	  Utils.invertMatrix4(this._tempMatrix4, cameraMatrix.elements);
-	  this._foaRotator.setRotationMatrix4(this._tempMatrix4);
+	  this._foaRotator.setRotationMatrix4(camera.matrix.elements);
 	};
 
 
@@ -2336,7 +2263,7 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	/**
 	 * Set the rotation matrix for the sound field rotation.
-	 * @param {Array} rotationMatrix      3x3 rotation matrix (row-major
+	 * @param {Array} rotationMatrix      3x3 rotation matrix (col-major
 	 *                                    representation)
 	 */
 	HOARenderer.prototype.setRotationMatrix = function(rotationMatrix) {
@@ -2348,16 +2275,13 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	/**
 	 * Update the rotation matrix from a Three.js camera object.
-	 * @param  {Object} cameraMatrix      The Matrix4 obejct of Three.js the camera.
+	 * @param  {Object} camera            The Three.js camera obejct.
 	 */
-	HOARenderer.prototype.setRotationMatrixFromCamera = function(cameraMatrix) {
+	HOARenderer.prototype.setRotationMatrixFromCamera = function(camera) {
 	  if (!this._isRendererReady)
 	    return;
 
-	  // Extract the inner array elements and inverse. (The actual view rotation is
-	  // the opposite of the camera movement.)
-	  Utils.invertMatrix4(this._tempMatrix4, cameraMatrix.elements);
-	  this._hoaRotator.setRotationMatrix4(this._tempMatrix4);
+	  this._hoaRotator.setRotationMatrix4(camera.matrix.elements);
 	};
 
 
